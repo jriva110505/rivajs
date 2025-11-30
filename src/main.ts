54528419a1a395b98b5fbc087ffe9ac225e2c25a
Jerome Riva <jriva@gbox.ncf.edu.ph>
2025-11-30 08:30:23 +0800

    Fix moduel

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
     dotenv.config();
     const app = await NestFactory.create(AppModule);
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 3300;
     app.enableCors();
     await app.listen(port);
     console.log(`Application is running on: http://localhost:${port}`);
